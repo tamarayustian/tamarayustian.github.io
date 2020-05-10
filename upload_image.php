4d54c908d25a2e3d2f2bebbfd34d497dc4d80997
Tamara Yustian <tamarayustian@gmail.com>
2020-05-10 17:46:58 +0800

    update upload image

--- upload_image.php
+++ upload_image.php
@@ -4,7 +4,7 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+if(isset($_POST["upload"])) {
 	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 	if($check !== false) 
 	{
