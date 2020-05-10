ea445bb2f1e67a71c95aab4fe268cf93e0804cfe
Tamara Yustian <tamarayustian@gmail.com>
2020-05-11 04:23:29 +0800

    change php to js

---
+++ upload.js
@@ -0,0 +1,21 @@
+const handleImageUpload = event => {
+  const files = event.target.files
+  const formData = new FormData()
+  formData.append('myFile', files[0])
+
+  fetch('/saveImage', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.path)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
+
+document.querySelector('#fileUpload').addEventListener('change', event => {
+  handleImageUpload(event)
+})
